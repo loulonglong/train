c279f39ee282c251c3235c108ead791f70efc7bc
loulonglong1125 <loulonglong1125@gmail.com>
2019-03-13 18:35:56 +0800

    douban_v0.3

---
+++ html/lesson5/my_carousel.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+        alert("HTML: " + $("#tv-1").html());
+
+        // 初始化轮播
+        $(".start-slide").click(function(){
+            $("#gallery-Carousel").carousel('cycle');
+        });
+        // 停止轮播
+        $(".pause-slide").click(function(){
+            $("#gallery-Carousel").carousel('pause');
+        });
+        // 循环轮播到上一个项目
+        $(".prev-slide").click(function(){
+            $("#gallery-Carousel").carousel('prev');
+        });
+        // 循环轮播到下一个项目
+        $(".next-slide").click(function(){
+            $("#gallery-Carousel").carousel('next');
+        });
+        // 循环轮播到某个特定的帧
+        $(".slide-one").click(function(){
+            $("#gallery-Carousel").carousel(0);
+        });
+        $(".slide-two").click(function(){
+            $("#gallery-Carousel").carousel(1);
+        });
+        $(".slide-three").click(function(){
+            $("#gallery-Carousel").carousel(2);
+        });
+
+
+});
---
+++ html/lesson5/tv_div_generation.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+        var textHtml ="<h4>惊奇队长 <span style='color: #e09015;'>8.7</span></h4>";
+        // var textHtml = ' <div class="col-md-3" style="padding: 10px;">
+        //                               <div class="thumbnail text-center" style="margin: 0;">
+        //                                 <a onclick="moreurl(this, )" href="https://movie.douban.com/subject/1652592/?from=showing">
+        //                                   <img alt="300x200" src="https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2541240741.webp" />
+        //                                   <h4>新喜剧之王 <span style="color: #e09015;">8.7</span></h4>
+        //                                 </a>
+        //                               </div>
+        //                             </div>
+        //                             <div class="col-md-3" style="padding: 10px;">
+        //                               <div class="thumbnail text-center" style="margin: 0;">
+        //                                 <a onclick="moreurl(this, )" href="https://movie.douban.com/subject/1652592/?from=showing">
+        //                                 <img src="https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2548870813.webp" alt="惊奇队长" rel="nofollow" class="">
+        //                                   <h4>惊奇队长 <span style="color: #e09015;">8.7</span></h4>
+        //                                 </a>
+        //                               </div>
+        //                             </div>
+        //                             <div class="col-md-3" style="padding: 10px;">
+        //                               <div class="thumbnail text-center" style="margin: 0;">
+        //                                 <a onclick="moreurl(this, )" href="https://movie.douban.com/subject/1652592/?from=showing">
+        //                                 <img src="https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2545472803.webp" alt="流浪地球" rel="nofollow" class="">
+        //                                   <h4>流浪地球 <span style="color: #e09015;">8.7</span></h4>
+        //                                 </a>
+
+        //                               </div>
+        //                             </div>
+        //                             <div class="col-md-3" style="padding: 10px;">
+        //                               <div class="thumbnail text-center" style="margin: 0;">
+        //                                 <a onclick="moreurl(this, )" href="https://movie.douban.com/subject/1652592/?from=showing">
+        //                                 <img src="https://img3.doubanio.com/view/photo/s_ratio_poster/public/p2546335362.webp" style="width: 94%;" alt="驯龙高手3" rel="nofollow" class="">
+        //                                   <h4>驯龙高手3 <span style="color: #e09015;">8.7</span></h4>
+        //                                 </a>
+        //                               </div>
+        //                             </div>';
+
+        $("#tv-1").html(textHtml));
+        console.log(textHtml);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
